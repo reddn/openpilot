48602c0f5a1eeb67f3e9e7d70820551987346606
Mathew Locoteta <m.locoteta@gmail.com>
2020-12-08 22:37:22 -0500

    added debug 1

--- panda/board/safety.h
+++ panda/board/safety.h
@@ -39,6 +39,7 @@
 #define SAFETY_SUBARU_LEGACY 22U
 #define SAFETY_HYUNDAI_LEGACY 23U
 #define SAFETY_HYUNDAI_COMMUNITY 24U
+#define ALLOW_DEBUG 1
 
 uint16_t current_safety_mode = SAFETY_SILENT;
 const safety_hooks *current_hooks = &nooutput_hooks;
@@ -249,7 +250,7 @@ const safety_hook_config safety_hook_registry[] = {
   {SAFETY_NISSAN, &nissan_hooks},
   {SAFETY_NOOUTPUT, &nooutput_hooks},
   {SAFETY_HYUNDAI_LEGACY, &hyundai_legacy_hooks},
-#ifdef ALLOW_DEBUG
+ifdef ALLOW_DEBUG 1
   {SAFETY_MAZDA, &mazda_hooks},
   {SAFETY_SUBARU_LEGACY, &subaru_legacy_hooks},
   {SAFETY_VOLKSWAGEN_PQ, &volkswagen_pq_hooks},
